793d6f7b2a4448289538ff09ba47db23a8326876
MYUNGHOON JU <myunghoonju@gmail.com>
2023-04-04 00:48:48 +0900

    add webserver
    tomcat

--- src/main/java/reboot/spring/BootApplication.java
+++ src/main/java/reboot/spring/BootApplication.java
@@ -1,8 +1,14 @@
 package reboot.spring;
 
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServer;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+
 public class BootApplication {
 
 	public static void main(String[] args) {
-		System.out.println("BootApplication");
+		ServletWebServerFactory factory = new TomcatServletWebServerFactory();
+		WebServer webServer = factory.getWebServer();
+		webServer.start();
 	}
 }
