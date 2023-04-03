76d7a5559fb9ad4a8f426d75094f54524f9e519d
MYUNGHOON JU <myunghoonju@gmail.com>
2023-04-04 00:39:06 +0900

    init commit

--- src/main/java/reboot/spring/BootApplication.java
+++ src/main/java/reboot/spring/BootApplication.java
@@ -1,13 +1,8 @@
 package reboot.spring;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
 public class BootApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BootApplication.class, args);
+		System.out.println("BootApplication");
 	}
-
 }
