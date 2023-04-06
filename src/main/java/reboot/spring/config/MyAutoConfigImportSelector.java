2a20d7c55f2afd8af4c2def33f05b4b58a9c8b6d
MYUNGHOON JU <myunghoonju@gmail.com>
2023-04-07 01:32:08 +0900

    add external import

--- src/main/java/reboot/spring/config/MyAutoConfigImportSelector.java
+++ src/main/java/reboot/spring/config/MyAutoConfigImportSelector.java
@@ -1,15 +1,26 @@
 package reboot.spring.config;
 
+import org.springframework.boot.context.annotation.ImportCandidates;
 import org.springframework.context.annotation.DeferredImportSelector;
 import org.springframework.core.type.AnnotationMetadata;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MyAutoConfigImportSelector implements DeferredImportSelector {
+
+    private final ClassLoader classLoader;
+
+    public MyAutoConfigImportSelector(ClassLoader classLoader) {
+        this.classLoader = classLoader;
+    }
 
     @Override
     public String[] selectImports(AnnotationMetadata importingClassMetadata) {
-        return new String[]{
-                "reboot.spring.config.autoConfig.DispatcherServletConfig",
-                "reboot.spring.config.autoConfig.TomcatWebServerConfig"
-        };
+        List<String> objects = new ArrayList<>();
+        Iterable<String> importCandidates = ImportCandidates.load(MyAutoConfig.class, classLoader);
+        importCandidates.forEach(objects::add);
+
+        return objects.toArray(String[]::new);
     }
 }
