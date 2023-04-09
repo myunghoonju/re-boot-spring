fbe731ce7135e8463c5c3bce5a6bea64e643ca49
MYUNGHOON JU <myunghoonju@gmail.com>
2023-04-09 20:38:01 +0900

    auto Conditional

--- src/main/java/reboot/spring/config/autoConfig/JettyWebServerConfig.java
+++ src/main/java/reboot/spring/config/autoConfig/JettyWebServerConfig.java
@@ -3,26 +3,14 @@ package reboot.spring.config.autoConfig;
 import org.springframework.boot.web.embedded.jetty.JettyServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Condition;
-import org.springframework.context.annotation.ConditionContext;
-import org.springframework.context.annotation.Conditional;
-import org.springframework.core.type.AnnotatedTypeMetadata;
 import reboot.spring.config.MyAutoConfig;
 
 @MyAutoConfig
-@Conditional(JettyWebServerConfig.JettyCondition.class)
+@ConditionalMyOnClass("org.eclipse.jetty.server.Server")
 public class JettyWebServerConfig {
 
     @Bean
     public ServletWebServerFactory jettyWebServerFactory() {
         return new JettyServletWebServerFactory();
-    }
-
-    static class JettyCondition implements Condition {
-
-        @Override
-        public boolean matches(ConditionContext context, AnnotatedTypeMetadata metadata) {
-            return true;
-        }
     }
 }
